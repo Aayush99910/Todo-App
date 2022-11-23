3710582fb14211e4b0301596ca7c9329985a7ec7
Aayush99910 <amaharjann01@gmail.com>
2022-11-23 11:47:46 +0545

    added better form design

--- src/Todo.js
+++ src/Todo.js
@@ -1,164 +1,160 @@
 let todosArray; // initailising the todosarray
-let todosArrayFromLocalStorage = JSON.parse(localStorage.getItem("myTodos")); // getting todos from the localStorage
+const todosArrayFromLocalStorage = JSON.parse(localStorage.getItem('myTodos')); // getting todos from the localStorage
 
-// if todos found in the localStorage we use that else 
+// if todos found in the localStorage we use that else
 // make a new empty array
 if (todosArrayFromLocalStorage) {
-    todosArray = todosArrayFromLocalStorage;
-}else {
-    todosArray = [];
+  todosArray = todosArrayFromLocalStorage;
+} else {
+  todosArray = [];
 }
 
 // DOM element
-const body = document.querySelector(".main-body-todos");
+const body = document.querySelector('.main-body-todos');
 
-// factory function which makes todo 
-export const createTodo = (title, dueDate, priority) => {
-    return {
-        title,
-        dueDate,
-        priority
-    }
-}
+// factory function which makes todo
+export const createTodo = (title, dueDate, priority, description) => ({
+  title,
+  dueDate,
+  priority,
+  description,
+});
 
-// this function pushes the object todo in the array 
-// and then saves the todoArray in the localStorage 
+// this function pushes the object todo in the array
+// and then saves the todoArray in the localStorage
 export const saveTodo = (todo) => {
-    todosArray.push(todo);
-    localStorage.setItem("myTodos", JSON.stringify(todosArray));
-}
+  todosArray.push(todo);
+  localStorage.setItem('myTodos', JSON.stringify(todosArray));
+};
 
-// adds more functionality 
-function moreFunctionality () {
-    const todoContainer = document.querySelectorAll(".todo-container");
-    todoContainer.forEach(function (eachtodoContainer) {
-        eachtodoContainer.addEventListener('click', () => {
-            const todoContainerBody = eachtodoContainer.querySelector(".todo-container-body");
-            const todoContainerHeading = eachtodoContainer.querySelector(".todo-container-heading");
-            todoContainerBody.classList.toggle("show-todo-container-body");
-            
-            const buttonContainer = todoContainerHeading.querySelector(".completed-delete-dropdown-container");
-            const dropDownBtn = buttonContainer.querySelector(".dropdown-btn");
-            dropDownBtn.classList.toggle("rotate");
-        });
+// adds dropDownFunctionality functionality
+function dropDownFunctionality() {
+  const todoContainer = document.querySelectorAll('.todo-container');
+  todoContainer.forEach((eachtodoContainer) => {
+    eachtodoContainer.addEventListener('click', () => {
+      const todoContainerBody = eachtodoContainer.querySelector('.todo-container-body');
+      const todoContainerHeading = eachtodoContainer.querySelector('.todo-container-heading');
+      todoContainerBody.classList.toggle('show-todo-container-body');
+
+      const buttonContainer = todoContainerHeading.querySelector('.completed-delete-dropdown-container');
+      const dropDownBtn = buttonContainer.querySelector('.dropdown-btn');
+      dropDownBtn.classList.toggle('rotate');
     });
+  });
 }
-
 
 // renders each todo
 function render(eachtodo) {
-    const todoContainer = document.createElement("div");
-    todoContainer.classList.add("todo-container");
+  const todoContainer = document.createElement('div');
+  todoContainer.classList.add('todo-container');
 
-    const todoContainerHeading = document.createElement("div");
-    todoContainerHeading.classList.add("todo-container-heading");
+  const todoContainerHeading = document.createElement('div');
+  todoContainerHeading.classList.add('todo-container-heading');
 
-    const title = document.createElement("p");
-    title.classList.add("title");
-    title.textContent = eachtodo.title;
+  const title = document.createElement('p');
+  title.classList.add('title');
+  title.textContent = eachtodo.title;
 
-    const buttonDiv = document.createElement("div");
-    buttonDiv.classList.add("completed-delete-dropdown-container");
-    
-    const completedBtn = document.createElement("button");
-    completedBtn.classList.add("completed-btn");
-    completedBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
-    
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete-btn");
-    deleteBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-    
-    const dropDown = document.createElement("button");
-    dropDown.classList.add("dropdown-btn");
-    dropDown.innerHTML = `<i class="fa-solid fa-angle-down"></i>`;
-    
-    buttonDiv.append(completedBtn, deleteBtn, dropDown);
+  const buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('completed-delete-dropdown-container');
 
-    const todoContainerBody = document.createElement("div");
-    todoContainerBody.classList.add("todo-container-body");
-    todoContainerBody.innerHTML = `
+  const completedBtn = document.createElement('button');
+  completedBtn.classList.add('completed-btn');
+  completedBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+  const dropDown = document.createElement('button');
+  dropDown.classList.add('dropdown-btn');
+  dropDown.innerHTML = '<i class="fa-solid fa-angle-down"></i>';
+
+  buttonDiv.append(completedBtn, deleteBtn, dropDown);
+
+  const todoContainerBody = document.createElement('div');
+  todoContainerBody.classList.add('todo-container-body');
+  todoContainerBody.innerHTML = `
         <p class="todo-body-title">Title: ${eachtodo.title}</p>
         <p class="todo-body-priority">Priority: ${eachtodo.priority}</p>
         <p class="todo-body-dueDate">DueDate: ${eachtodo.dueDate}</p>
-        <p class="todo-body-description">Description: Go to temple at 5 pm.</p>
+        <p class="todo-body-description">Description: ${eachtodo.description}</p>
     `;
+  /*eslint-disable */
+  if (eachtodo.priority.toLowerCase() == 'low') {
+    todoContainerHeading.classList.add('green');
+  } else if (eachtodo.priority.toLowerCase() == 'medium') {
+    todoContainerHeading.classList.add('yellow');
+  } else if (eachtodo.priority.toLowerCase() == 'high') {
+    todoContainerHeading.classList.add('red');
+  }
 
-    if (eachtodo.priority.toLowerCase() == "low") {
-        todoContainerHeading.classList.add("green");
-    } else if (eachtodo.priority.toLowerCase() == "medium") {
-        todoContainerHeading.classList.add("yellow");
-    } else if (eachtodo.priority.toLowerCase() == "high") {
-        todoContainerHeading.classList.add("red");
-    }
-
-    todoContainerHeading.append(title, buttonDiv);
-    todoContainer.append(todoContainerHeading, todoContainerBody);
-    body.append(todoContainer);
+  todoContainerHeading.append(title, buttonDiv);
+  todoContainer.append(todoContainerHeading, todoContainerBody);
+  body.append(todoContainer);
 }
 
 // renderTodo takes a argument
 // renders according to the argument
 // if choice is all renders all the todos
 // if choice is today renders all the todos due today
+/*eslint-disable */
 function _renderTodos(choice) {
-    const bodyHeading = document.getElementById("heading");
-    body.innerHTML = " ";
-    if (todosArray.length === 0) {
-        const div = document.createElement("div");
-        div.classList.add("todo-container");
-        div.style.justifyContent = "center";
-        const p = document.createElement("p");
-        p.classList.add("empty-todos");
-        p.textContent = "There is currently no task added.";
-        div.append(p);
-        body.append(div);
-    }
+  const bodyHeading = document.getElementById('heading');
+  body.innerHTML = ' ';
+  if (todosArray.length === 0) {
+    const div = document.createElement('div');
+    div.classList.add('todo-container');
+    div.style.justifyContent = 'center';
+    const p = document.createElement('p');
+    p.classList.add('empty-todos');
+    p.textContent = 'There is currently no task added.';
+    div.append(p);
+    body.append(div);
+  }
 
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth() + 1;
-    const currentday= new Date().getDate(); 
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  const currentday = new Date().getDate();
 
-    if (choice == "all") {
-        bodyHeading.textContent = "Inbox";
-        todosArray.forEach(function(eachtodo) {
-            render(eachtodo);
-        })
-        moreFunctionality();
-    }
-    else if (choice == "today") {
-        bodyHeading.textContent = "Today";
-        const todayDay = `${currentYear}-${currentMonth}-${currentday}`;
+  if (choice == 'all') {
+    bodyHeading.textContent = 'Inbox';
+    todosArray.forEach((eachtodo) => {
+      render(eachtodo);
+    });
+    dropDownFunctionality();
+  } else if (choice == 'today') {
+    bodyHeading.textContent = 'Today';
+    const todayDay = `${currentYear}-${currentMonth}-${currentday}`;
 
-        todosArray.forEach(function(eachtodo) {
-            if (todayDay == eachtodo.dueDate) {
-                render(eachtodo);
-            }
-        })
-        moreFunctionality();
-    }
-    else if (choice == "upcoming") {
-        bodyHeading.textContent = "Upcoming Tasks";
+    todosArray.forEach((eachtodo) => {
+      if (todayDay == eachtodo.dueDate) {
+        render(eachtodo);
+      }
+    });
+    dropDownFunctionality();
+  } else if (choice == 'upcoming') {
+    bodyHeading.textContent = 'Upcoming Tasks';
 
-        todosArray.forEach(function(eachtodo) {
-            let fulldate = eachtodo.dueDate.split("-");
-            const [year, month, day] = fulldate;
-            if (year > currentYear || month > currentMonth || day > currentday) {
-                render(eachtodo);
-            }
-        });
-        moreFunctionality();
-    }
+    todosArray.forEach((eachtodo) => {
+      const fulldate = eachtodo.dueDate.split('-');
+      const [year, month, day] = fulldate;
+      if (year > currentYear || month > currentMonth || day > currentday) {
+        render(eachtodo);
+      }
+    });
+    dropDownFunctionality();
+  }
 }
-
 
 export const renderTodos = () => {
-    _renderTodos("all");
-}
+  _renderTodos('all');
+};
 
 export function renderTodayTodos() {
-    _renderTodos("today");
+  _renderTodos('today');
 }
 
 export const renderUpcomingTodos = () => {
-    _renderTodos("upcoming");
-} 
+  _renderTodos('upcoming');
+};
--- src/index.js
+++ src/index.js
@@ -1,85 +1,86 @@
-import { createTodo, saveTodo, renderTodos, renderTodayTodos, renderUpcomingTodos } from "./Todo.js";
-import "./styles/main.css";
-
-
+/*eslint-disable */
+import {
+  createTodo, saveTodo, renderTodos, renderTodayTodos, renderUpcomingTodos,
+} from './Todo.js';
+import './styles/main.css';
 
 // DOM elements
-const modal = document.querySelector("#modal");
-const addBtn = document.querySelector("#add-task-btn");
-const cancelBtn = document.querySelector("#cancel");
-const form = document.querySelector("form");
-const inbox = document.querySelector("#inbox");
-const today = document.querySelector("#today");
-const upcomingTask = document.querySelector("#upcoming");
+const modal = document.querySelector('#modal');
+const addBtn = document.querySelector('#add-task-btn');
+const cancelBtn = document.querySelector('#cancel');
+const form = document.querySelector('form');
+const inbox = document.querySelector('#inbox');
+const today = document.querySelector('#today');
+const upcomingTask = document.querySelector('#upcoming');
 
 // event listener for add task btn
-addBtn.addEventListener("click", () => {
-    modal.showModal();
+addBtn.addEventListener('click', () => {
+  modal.showModal();
+  dropDownFunctionality();
 });
-
 
 // event listener for cancel btn
-cancelBtn.addEventListener("click", () => {
-    modal.close();
+cancelBtn.addEventListener('click', () => {
+  modal.close();
 });
 
-
 // event listener for form
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const titleInput = document.querySelector("#title");
-    const dueDateInput = document.querySelector("#dueDate");
-    const priorityInput = document.querySelector("#priority");
-        
-    // if empty form is submitted user is alerted about it
-    if (titleInput.value === "" || dueDateInput.value === "" || priorityInput.value ===  "") {
-        alert("Please fill the form.");
-        return; // skips the whole other code after this
-    }
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const titleInput = document.querySelector('#title');
+  const dueDateInput = document.querySelector('#dueDate');
+  const priorityInput = document.querySelector('#priority');
+  const descriptionInput = document.querySelector("#description");
 
-    // when submitted closes the modal and creates the todo by
-    // calling the createTodo factory function which is 
-    // imported from Todo.js
-    // then calls saveTodo function which saves the todo
-    // in the localStorage lastly it renders them
-    modal.close();
-    const todo = createTodo(titleInput.value, dueDateInput.value, priorityInput.value);
-    saveTodo(todo);
-    renderTodos();
+  // if empty form is submitted user is alerted about it
+  if (titleInput.value === '' || dueDateInput.value === '' || priorityInput.value === '' || descriptionInput.value === '') {
+    /*eslint-disable */
+    alert('Please fill the form.'); 
+    return; // skips the whole other code after this
+  }
+
+  // when submitted closes the modal and creates the todo by
+  // calling the createTodo factory function which is
+  // imported from Todo.js
+  // then calls saveTodo function which saves the todo
+  // in the localStorage lastly it renders them
+  modal.close();
+  const todo = createTodo(titleInput.value, dueDateInput.value, priorityInput.value, descriptionInput.value);
+  saveTodo(todo);
+  renderTodos();
 });
 
 // when today is clicked on the sidebar renderTodayTodos
 // is called which renders all the todo duedated today
-today.addEventListener("click", () => {
-    renderTodayTodos();
+today.addEventListener('click', () => {
+  renderTodayTodos();
 });
 
 // renders all the todos
-inbox.addEventListener("click", () => {
-    renderTodos();
+inbox.addEventListener('click', () => {
+  renderTodos();
 });
 
 // renders all the upcoming todos
-upcomingTask.addEventListener("click", () => {
-    renderUpcomingTodos();
+upcomingTask.addEventListener('click', () => {
+  renderUpcomingTodos();
 });
-
 
 // when the user logs in the pade we render the todos
 renderTodos();
 
-
-const todoContainer = document.querySelectorAll(".todo-container");
-
-
-todoContainer.forEach(function (eachtodoContainer) {
+// adds dropDownFunctionality functionality
+function dropDownFunctionality() {
+  const todoContainer = document.querySelectorAll('.todo-container');
+  todoContainer.forEach((eachtodoContainer) => {
     eachtodoContainer.addEventListener('click', () => {
-        const todoContainerBody = eachtodoContainer.querySelector(".todo-container-body");
-        const todoContainerHeading = eachtodoContainer.querySelector(".todo-container-heading");
-        todoContainerBody.classList.toggle("show-todo-container-body");
-        
-        const buttonContainer = todoContainerHeading.querySelector(".completed-delete-dropdown-container");
-        const dropDownBtn = buttonContainer.querySelector(".dropdown-btn");
-        dropDownBtn.classList.toggle("rotate");
+      const todoContainerBody = eachtodoContainer.querySelector('.todo-container-body');
+      const todoContainerHeading = eachtodoContainer.querySelector('.todo-container-heading');
+      todoContainerBody.classList.toggle('show-todo-container-body');
+
+      const buttonContainer = todoContainerHeading.querySelector('.completed-delete-dropdown-container');
+      const dropDownBtn = buttonContainer.querySelector('.dropdown-btn');
+      dropDownBtn.classList.toggle('rotate');
     });
-});
+  });
+}
